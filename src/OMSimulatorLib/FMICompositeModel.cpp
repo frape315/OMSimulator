e882fcd3525ff40e361ed9b3d598cbe22205cc2b
Frank Petersson <frape315@student.liu.se>
2018-09-03 14:39:51 +0200

    added getStates in solver to make sure we can acess the current states in the composite model

--- src/OMSimulatorLib/FMICompositeModel.cpp
+++ src/OMSimulatorLib/FMICompositeModel.cpp
@@ -1148,7 +1148,8 @@ oms_status_enu_t oms2::FMICompositeModel::stepUntilVariableStep(ResultWriter& re
       {
         it.second->doStep(halftime);
         it.second->doStep(time);
-      }
+	  }
+
     if (realtime_sync)
     {
       auto now = std::chrono::steady_clock::now();
