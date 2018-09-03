c32ecbd71182cab3c7e69d6f1794a18a308875ae
Frank Petersson <frape315@student.liu.se>
2018-09-03 10:41:33 +0200

    started adding variable timestep, commiting so I can pull

--- src/OMSimulatorLib/FMICompositeModel.h
+++ src/OMSimulatorLib/FMICompositeModel.h
@@ -134,6 +134,8 @@ namespace oms2
     oms_status_enu_t registerSignalsForResultFile(ResultWriter& resultWriter);
     oms_status_enu_t emit(ResultWriter& resultWriter);
 
+    
+
     oms_status_enu_t stepUntilStandard(ResultWriter& resultWriter, double stopTime, double communicationInterval, double loggingInterval, bool realtime_sync);
     oms_status_enu_t stepUntilVariableStep(ResultWriter& resultWriter, double stopTime, double communicationInterval, double loggingInterval, bool realtime_sync);
 #if !defined(__arm__)
