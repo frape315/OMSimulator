46066428d601918f761b8e3e7e076925d23a16f1
Frank Petersson <frape315@student.liu.se>
2018-09-03 10:41:33 +0200

    started adding variable timestep, commiting so I can pull

--- src/OMSimulatorLib/FMICompositeModel.h
+++ src/OMSimulatorLib/FMICompositeModel.h
@@ -131,7 +131,8 @@ namespace oms2
     oms_status_enu_t registerSignalsForResultFile(ResultWriter& resultWriter);
     oms_status_enu_t emit(ResultWriter& resultWriter);
 
-    oms_status_enu_t stepUntilStandard(ResultWriter& resultWriter, double stopTime, double communicationInterval, double loggingInterval, bool realtime_sync);
+    oms_status_enu_t stepUntilStandard(ResultWriter& resultWriter, double stopTime, double communicationInterval, double loggingInterval, bool realtime_sync);	
+	oms_status_enu_t stepUntilVariableStep(ResultWriter& resultWriter, double stopTime, double communicationInterval, double loggingInterval, bool realtime_sync)
     oms_status_enu_t stepUntilPCTPL(ResultWriter& resultWriter, double stopTime, double communicationInterval, double loggingInterval, bool realtime_sync);
     oms_status_enu_t updateDependencyGraphs();
 
--- src/OMSimulatorLib/Scope.cpp
+++ src/OMSimulatorLib/Scope.cpp
@@ -1651,6 +1651,10 @@ oms_status_enu_t oms2::Scope::setMasterAlgorithm(const ComRef& cref, const std::
   {
     model->setMasterAlgorithm(oms2::MasterAlgorithm::PMRCHANNELM);
   }
+    else if (masterAlgorithm == "variablestep")
+  {
+	model->setMasterAlgorithm(oms2::MasterAlgorithm::VARIABLESTEP);  
+  }
   else {
     std::string message = std::string("Unsupported master algorithm ")
       + masterAlgorithm + std::string("\nFollowing master algorithms are supported:\n")
@@ -1658,6 +1662,7 @@ oms_status_enu_t oms2::Scope::setMasterAlgorithm(const ComRef& cref, const std::
       + std::string("standard, pmrchannela.\n")
       + std::string("standard, pmrchannelcv.\n")
       + std::string("standard, pmrchannelm.\n")
+      + std::string("variablestep.\n")
       + std::string("Defaulting to \"standard\"!\n");
     logWarning(message);
     model->setMasterAlgorithm(oms2::MasterAlgorithm::STANDARD);
