5cc4604bebd5d43fa34d9d921542da9d9820413c
Frank Petersson <frape315@student.liu.se>
2018-09-04 11:32:38 +0200

    added some comments and such to FMIComp, for the variabletimestep. Still WIP

--- src/OMSimulatorLib/FMICompositeModel.h
+++ src/OMSimulatorLib/FMICompositeModel.h
@@ -134,11 +134,9 @@ namespace oms2
     oms_status_enu_t registerSignalsForResultFile(ResultWriter& resultWriter);
     oms_status_enu_t emit(ResultWriter& resultWriter);
 
-    
-
     oms_status_enu_t stepUntilStandard(ResultWriter& resultWriter, double stopTime, double communicationInterval, double loggingInterval, bool realtime_sync);
-    oms_status_enu_t stepUntilVariableStep(ResultWriter& resultWriter, double stopTime, double communicationInterval, double loggingInterval, bool realtime_sync)
-#if !defined(__arm__)    
+    oms_status_enu_t stepUntilVariableStep(ResultWriter& resultWriter, double stopTime, double communicationInterval, double loggingInterval, bool realtime_sync);
+#if !defined(__arm__)
     oms_status_enu_t stepUntilPCTPL(ResultWriter& resultWriter, double stopTime, double communicationInterval, double loggingInterval, bool realtime_sync);
 #endif // if !defined(__arm__)
     oms_status_enu_t updateDependencyGraphs();
