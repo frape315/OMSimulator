07e942ad66a1bd6685d302d4d777dc5a517b089b
Frank Petersson <frape315@student.liu.se>
2018-10-05 16:01:44 +0200

    fixed a spelling error in Nominal, and a wrong variable name in tolerance

--- src/OMSimulatorLib/FMICompositeModel.cpp
+++ src/OMSimulatorLib/FMICompositeModel.cpp
@@ -1109,7 +1109,6 @@ oms_status_enu_t oms2::FMICompositeModel::stepUntilVariableStep(ResultWriter& re
 			states_smallstep = it.second->getStates();
 			states_smallstep_der = it.second->getStatesDer();			 // Do we need this?
 			states_smallstep_nominal = it.second->getStatesNominal();	 // Do we need this?
-
 			for (int i=0; i<states_bigstep.size(); ++i)
 			 {
          double small = *states_smallstep[i];
